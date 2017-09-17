434e11a3a25951ef35ab2c5a38de1235b8c7bbf8
lorstevens <lornacstevens@gmail.com>
2017-09-17 16:58:47 -0500

    html updates

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCcEpbcqJ7jLtvH_A4sS-Y9fYE7dwDz6bw",
+    authDomain: "lornaproject-cbbec.firebaseapp.com",
+    databaseURL: "https://lornaproject-cbbec.firebaseio.com",
+    projectId: "lornaproject-cbbec",
+    storageBucket: "lornaproject-cbbec.appspot.com",
+    messagingSenderId: "679728535251"
+  };
+  firebase.initializeApp(config);
